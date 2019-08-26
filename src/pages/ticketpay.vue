5118bb68bdd6d2bf15d7fbf4e07d3a110bd5a7d3
caichenghui <caich_web@163.com>
2019-08-26 18:07:04 +0800

    增加sass

--- src/pages/ticketpay.vue
+++ src/pages/ticketpay.vue
@@ -3,7 +3,7 @@
     <div class="activityheader">
       <div class="accontenty">
         <p>韩辰医疗美容鼻模海选</p>
-          <p class="syyouxi">使用有效期</p>
+        <p class="syyouxi">使用有效期</p>
         <p class="tdshp"><span class="addressdeta1">高新二路129号创意产业园3037号</span> <span class="pricetick">¥8888</span></p>
         <p class="goumai">
           <span class="goumaisl">购买数量</span>
@@ -20,60 +20,59 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
   .activitydetail{
     padding: 44px 30px 0;
-  }
-  .activityheader{
-    border-radius: 8px;
-  }
-
-.accontenty{
-  padding-bottom: 12px;
-}
-  .activitydetail .accontenty p{
-    line-height: 44px;
-    padding: 22px 0 22px 0;
-    margin-bottom: 0;
-    border-bottom: 1px solid #F3F3F3;
-    width: 100%;
-  }
-  .activitydetail .accontenty .syyouxi{
-    padding: 16px 0 0 0 ;
-    font-size:30px;
-    border: none;
-  }
-  .activitydetail .accontenty  .tdshp{
-    border: none;
-    padding: 0;
-    display: -webkit-box;
-    display: -webkit-flex;
-    display: flex;
-    -webkit-box-orient: horizontal;
-    -webkit-box-direction: normal;
-    -webkit-flex-direction: row;
-    flex-direction: row;
-    -webkit-box-align: center;
-    -webkit-align-items: center;
-    align-items: center;
-  }
-  .tdshp .addressdeta1{
-    color: #666666;
-    font-size: 24px;
-    padding: 0;
-    width: 100%;
-  }
-  .tdshp  .pricetick{
-    margin-left: 20px;
-    color: #999;
-    color: #F4781D;
-    font-size: 28px;
-    font-weight: bold;
-  }
-  .activitydetail .accontenty .goumai{
-    padding: 20px 0 20px 0 ;
-    font-size:30px;
-    border: none;
-    width: 100%;
+    .activityheader{
+      border-radius: 8px;
+      .accontenty{
+        padding-bottom: 12px;
+        p{
+          line-height: 44px;
+          padding: 22px 0 22px 0;
+          margin-bottom: 0;
+          border-bottom: 1px solid #F3F3F3;
+          width: 100%;
+        }
+        .syyouxi{
+          padding: 16px 0 0 0 ;
+          font-size:30px;
+          border: none;
+        }
+        .tdshp{
+          border: none;
+          padding: 0;
+          display: -webkit-box;
+          display: -webkit-flex;
+          display: flex;
+          -webkit-box-orient: horizontal;
+          -webkit-box-direction: normal;
+          -webkit-flex-direction: row;
+          flex-direction: row;
+          -webkit-box-align: center;
+          -webkit-align-items: center;
+          align-items: center;
+          .addressdeta1{
+            color: #666666;
+            font-size: 24px;
+            padding: 0;
+            width: 100%;
+          }
+          .pricetick{
+            margin-left: 20px;
+            color: #999;
+            color: #F4781D;
+            font-size: 28px;
+            font-weight: bold;
+          }
+        }
+        .goumai{
+          padding: 20px 0 20px 0 ;
+          font-size:30px;
+          border: none;
+          width: 100%;
+        }
+      }
+    }
   }
 </style>
