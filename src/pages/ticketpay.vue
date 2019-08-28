008d48c9ca196a73315e53c36f159feb468867ed
lili wu <xxxwulili@gmail.com>
2019-08-28 23:41:18 +0800

    门票订单相关

--- src/pages/ticketpay.vue
+++ src/pages/ticketpay.vue
@@ -7,9 +7,9 @@
         <p class="tdshp"><span class="addressdeta1">{{ticketDetail.end_time}}</span> <span class="pricetick">¥{{ticketDetail.price}}</span></p>
         <p class="goumai">
           <span class="goumaisl">购买数量</span>
-        <span class="minus"><i class="row"></i></span>
-        <input class="text" type="tel" value="1" id="buyNum1">
-        <span class="plus" ><i class="row"></i><i class="col"></i></span>
+        <span @click="minusnum()" class="minus"><i class="row"></i></span>
+          <span class="shownum">{{number}}</span>
+        <span  @click="addnum()" class="plus" ><i class="row"></i><i class="col"></i></span>
         </p>
       </div>
     </div>
@@ -18,45 +18,150 @@
         <p>游客信息 <span class="tip">需填<i>1</i>位，用于入园身份证</span> </p>
         <p class="info">
           <label for="cstname">姓名</label>
-          <input id="cstname" type="text" placeholder="必填，输入证件上的姓名">
+          <input v-model="true_name" id="cstname" type="text" placeholder="必填，输入证件上的姓名">
         </p>
-        <p class="info"  v-if="ticketDetail">
+        <p class="info"  v-if="ticketDetail.identity==2">
           <label for="csttype">证件号</label>
-          <input maxlength="11" id="csttype" type="text" placeholder="必填，输入联系人证件号">
+          <input v-model="idcard" id="csttype" type="text" placeholder="必填，输入联系人证件号">
         </p>
         <p class="info">
           <label for="csttel">手机号</label>
-          <input maxlength="11" id="csttel" type="text" placeholder="必填，输入联系人手机号">
+          <input v-model="mobile" maxlength="11" id="csttel" type="text" placeholder="必填，输入联系人手机号">
         </p>
       </div>
     </div>
-    <Paybutton @pay="payClient"/>
+    <Paybutton :price="orderMoney" @pay="handlepay"/>
   </div>
 </template>
 
 <script>
-import {ticketPaylApi} from '@/api'
+import {ticketPaylApi, mallticketOrderBuy} from '@/api'
+import getSitem from '@/utils/storage'
 import Paybutton from '../components/paybutton.vue'
+const BigNumber = require('bignumber.js')
 export default {
+  data () {
+    return {
+      goods_id: null,
+      ticketDetail: null,
+      number: 1,
+      price: 0,
+      total: 0,
+      mobile: '',
+      true_name: '',
+      idcard: '',
+      identity: null,
+      wxpay: {}
+    }
+  },
   components: {
     Paybutton
   },
   mounted () {
     this.getTicketDetail()
   },
+  computed: {
+    orderMoney () {
+      var price = new BigNumber(this.price)
+      return price.multipliedBy(this.number).toFixed()
+    }
+  },
   methods: {
-    payClient (e) {
-      console.log(e)
+    onBridgeReady () {
+      WeixinJSBridge.invoke(
+        // 'getBrandWCPayRequest', {
+        //   'appId': data.appId, // 公众号名称，由商户传入
+        //   'timeStamp': data.timeStamp, // 时间戳，自1970年以来的秒数
+        //   'nonceStr': data.nonceStr, // 随机串
+        //   'package': data.package,
+        //   'signType': data.signType, // 微信签名方式：
+        //   'paySign': data.paySign // 微信签名
+        // },
+        'getBrandWCPayRequest', this.wxpay,
+        function (res) {
+          console.log(res)
+          if (res.err_msg === 'get_brand_wcpay_request:ok') {
+            // 使用以上方式判断前端返回,微信团队郑重提示：
+            // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
+          }
+        })
     },
     async getTicketDetail () {
       let formdata = {goods_id: this.$route.query.id}
-      // let token=setStr("token")
-      let token = 'b9a0bf511d1522999f74c78feb898d97f18d4de1f5e20828c1f9cc2ea7dd8e0d'
+      let token = getSitem.getStr('token') ? getSitem.getStr('token') : 'b9a0bf511d1522999f74c78feb898d97f18d4de1f5e20828c1f9cc2ea7dd8e0d'
       formdata.token = token
       const data = await ticketPaylApi(formdata)
       if (data.code === 1) {
         this.ticketDetail = data.data
+        this.price = data.data.price
+        this.goods_id = data.data.goods_id
+        this.identity=data.data.identity
       }
+    },
+    async mallOrderBuyApi () {
+      const data = {
+        goods_id: this.goods_id,
+        num: this.number,
+        token: getSitem.getStr('token'),
+        mobile: this.mobile,
+        true_name: this.true_name,
+        idcard: this.idcard
+      }
+      if (!this.checkName(this.true_name)) { return }
+      if (this.identity === 2) {
+        if (!this.checkCard(this.idcard)) { return }
+      }
+      if (!this.checkTel(this.mobile)) { return }
+      const result = await mallticketOrderBuy(data)
+      if (result.code === 1) {
+        console.log(result)
+        if (typeof WeixinJSBridge === 'undefined') {
+          if (document.addEventListener) {
+            document.addEventListener('WeixinJSBridgeReady', this.onBridgeReady, false)
+          } else if (document.attachEvent) {
+            document.attachEvent('WeixinJSBridgeReady', this.onBridgeReady)
+            document.attachEvent('onWeixinJSBridgeReady', this.onBridgeReady)
+          }
+        } else {
+          this.wxpay = result.data.wxpay
+          this.onBridgeReady()
+        }
+      } else {
+        this.$toast({message: result.msg, duration: 2000})
+      }
+    },
+    handlepay (e) {
+      this.mallOrderBuyApi()
+    },
+    addnum () {
+      this.number++
+    },
+    minusnum () {
+      if (this.number === 1) {
+        return
+      }
+      this.number--
+    },
+    checkTel (str) {
+      let reg = /^\d{11,15}$/
+      if (!reg.test(str)) {
+        this.$toast({message: '电话号码不规范，请修改！', duration: 2000})
+      }
+      return reg.test(str)
+    },
+    checkCard (str) {
+      let reg = /^[\d|x]{17}[\d|X|x]$/
+      if (!reg.test(str)) {
+        this.$toast({message: '身份证格式不正确，请修改！', duration: 2000})
+      }
+      return reg.test(str)
+    },
+    checkName (str) {
+      let reg = /\S/
+      if (!reg.test(str)) {
+        this.$toast({message: '请输入姓名！', duration: 2000})
+      }
+      return reg.test(str)
     }
   }
 }
@@ -150,7 +255,7 @@ export default {
             -webkit-align-items: center;
             align-items: center;
           }
-          .minus,.plus{
+          .minus,.plus,.shownum{
             position: relative;
             max-width: 40px;
             min-width: 40px;
@@ -180,6 +285,11 @@ export default {
               height: 20px;
               background-color: #8C3B92;
             }
+          }
+          .shownum{
+            background: #fff;
+            max-width: 80px;
+            width: 80px;
           }
           .text{
             text-align: center;
