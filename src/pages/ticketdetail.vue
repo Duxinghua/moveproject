554780422ebadb7feacf55f9178f62e66bc32b75
lili wu <xxxwulili@gmail.com>
2019-08-29 00:31:13 +0800

    分享功能

--- src/pages/ticketdetail.vue
+++ src/pages/ticketdetail.vue
@@ -9,7 +9,7 @@
           <div class="tdpfl">
             <span>活动价</span><span>¥{{ticketDetail.price}}</span><span>¥{{ticketDetail.price_cost}}</span>
           </div>
-          <div class="tdpfr sharebutton">
+          <div class="tdpfr sharebutton" @click="shareClick">
             <img src="../assets/images/share.png"/>
             <span>分享赚佣金</span>
           </div>
@@ -44,22 +44,117 @@
 </template>
 
 <script>
-import {ticketDetailApi} from '@/api'
+import {ticketDetailApi, weixinGetShare} from '@/api'
 export default {
   data () {
     return {
-      ticketDetail: null
+      ticketDetail: null,
+      goods_id: null,
+      appid:'wx505f185e9f5fcf57',
+      wxpay:null
     }
   },
   mounted () {
     this.getTicketDetail()
+    this.weixinGetShareApi()
   },
   methods: {
+    shareClick(){
+      console.log('ss')
+      const data = {
+        img: 'https://xinbr.qixiuu.com/upload/default/logo.png'
+      }
+
+    },
+    wxs(wxpay) {
+      wx.config({
+        debug: true,
+        appId: wxpay.appId,
+        timestamp: wxpay.timestamp,
+        nonceStr: wxpay.nonceStr,
+        signature: wxpay.signature,
+        jsApiList: [
+          'checkJsApi',
+          'onMenuShareTimeline',
+          'onMenuShareAppMessage'
+        ]
+      });
+      wx.error(function (res) {
+        console.log('error',res)
+      });
+      // 在这里调用 API
+      wx.ready(function () {
+        wx.checkJsApi({
+          jsApiList: [
+            'checkJsApi',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage'
+          ],
+          success: function (res) {
+            //alert(JSON.stringify(res));
+          }
+        });
+
+        //点击分享到朋友圈
+        wx.onMenuShareTimeline({
+          title: '无人茶社邀您喝好茶！', // 分享标题
+          desc: '邀您喝好茶、邀您体验无人值守！', // 分享描述
+          link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: wxpay.logo, // 分享图标
+          trigger: function (res) {
+            // 不要尝试在trigger中使用ajax异步请求修改本次分享的内容，因为客户端分享操作是一个同步操作，这时候使用ajax的回包会还没有返回
+            alert('用户点击分享到朋友圈');
+          },
+          success: function () {
+            // 用户确认分享后执行的回调函数
+            alert('分享成功');
+
+          },
+          cancel: function () {
+            // 用户取消分享后执行的回调函数
+            alert('分享取消');
+          },
+          fail: function (res) {
+            alert(JSON.stringify(res));
+          }
+        });
+        wx.onMenuShareAppMessage({
+          title: '无人茶社邀您喝好茶！', // 分享标题
+          desc: '邀您喝好茶、邀您体验无人值守！', // 分享描述
+          link: location.href, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+          imgUrl: wxpay.logo, // 分享图标
+          type: 'link', // 分享类型,music、video或link，不填默认为link
+          dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
+          success: function () {
+            // 用户确认分享后执行的回调函数
+            // alert('分享成功');
+
+          },
+          cancel: function () {
+            // 用户取消分享后执行的回调函数
+            // alert('分享取消');
+          }
+        });
+      })
+
+    },
+    async weixinGetShareApi () {
+      const data = {
+        appid: this.appid,
+        current_url: location.href
+      }
+      const result = await weixinGetShare(data)
+      if(result.code === 1){
+        this.wxpay = result.data
+        this.wxs(result.data)
+      }
+    },
     async getTicketDetail () {
       let formdata = {goods_id: this.$route.query.id}
       const data = await ticketDetailApi(formdata)
       if (data.code === 1) {
         this.ticketDetail = data.data
+        this.goods_id = data.data.goods_id
       }
     }
   }
